594de4b4bdde0071cfd5c1150a563d2e63a2de30
udamadu11 <2017is048@stu.ucsc.cmb.ac.lk>
2019-11-09 08:55:04 +0530

    update new 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'recepe';
+  optionLoaded = "recepes";
+  onNavigation(option:string){
+    this.optionLoaded = option;
+  }
 }
